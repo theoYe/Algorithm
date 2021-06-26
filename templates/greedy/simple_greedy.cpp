cf0649548e4623265cd5318873995b69069a78ac
伟伟 叶 <634948676@qq.com>
2021-06-26 22:04:48 +0800

    Feature: start to coding greedy

--- templates/greedy/simple_greedy.cpp
+++ templates/greedy/simple_greedy.cpp
@@ -2,3 +2,57 @@
 // Created by dell on 2021/6/26.
 //
 
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct mooncake{
+    double store; //库存
+    double sell; //总售价
+    double price; // 单价
+}cake[1001];
+
+
+
+bool cmp(mooncake  a, mooncake b){
+    return a.price > b.price;
+}
+
+
+int main(){
+    int n;
+    double D;
+
+    cin >> n >> D;
+
+    for(int i= 0 ; i < n; i ++){
+        cin >> cake[i].store;
+    }
+
+    for(int i = 0;  i < n; i ++){
+
+        cin >> cake[i].sell;
+        cake[i].price = cake[i].sell / cake[i].store;  //计算单价
+    }
+
+
+    sort(cake , cake + n, cmp);
+    double ans = 0; //收益
+
+    for(int i =0 ; i < n ; i ++){
+        if(cake[i].store <= D){   // 如果需求量高于月饼需求量
+            D-= cake[i].store;    //第 i 种月饼全部卖出
+            ans += cake[i].sell;
+
+        }else{   //如果月饼库存量高于需求量
+            ans += cake[i].price * D;  //只卖出剩余需求量的月饼
+            break;
+        }
+    }
+
+    printf("%.2f\n", ans);
+    return 0;
+}
+
+
---
+++ terms/greedy/4-4-A.cpp
@@ -0,0 +1,45 @@
+//
+// Created by dell on 2021/6/26.
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+struct tvShow{
+    int si; //开始时间
+    int ei; //结束时间
+}tv[1001];
+
+//左边从大到小排序
+int cmp(tvShow t1, tvShow t2){
+    if(t1.si != t2.si) return t1.si > t2.si;
+    else return t1.si < t2.si;  //相同时按照右端点从小到大排序
+}
+
+int main(){
+
+    int n, c;
+
+    while(cin >> n , n){
+        for(int i = 0;  i < n; i ++){
+            cin >> tv[i].si >> tv[i].ei;
+        }
+
+
+
+
+        int arr[100];
+
+        for(int i = 0;  i < 100; i ++){
+            arr[i] = 0;
+        }
+
+    }
+
+
+
+    return 0;
+}
+
